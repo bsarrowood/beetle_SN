3ea85cb61c09641e172289998a1cd2b2fcb7cb8b
Brad Arrowood <61139407+bsarrowood@users.noreply.github.com>
2022-05-17 11:17:42 -0500

    Update beetle_SN.py
    updated script to prompt user for file to be used instead of hardcoding the file name, added the clear func back in, and adding some info to be printed to the screen including the total word count of the file, total number of unique words found, and how many common words from the list were removed. i will be also removing the code which removes any entries of 5 or less occurrences as the script is shifting to compare 2 files (latest data dump to older data dump) and instead output the difference for each word frequency to better show which words have been mentioned more frequently since the last dataset and which may have gone down. after this i still plan it to make a word cloud with the words who have had the highest uptrend in usage and a bar chat to show more detail.

--- beetle_SN.py
+++ beetle_SN.py
@@ -1,6 +1,6 @@
 # Created by:       Brad Arrowood
 # Created on:       2022.05.09
-# Last updated:     2022.05.16
+# Last updated:     2022.05.17
 # Script name:      beetle_SN.py
 # Description:      script to return top 50 for word frequency count of a file, less common words
 #
@@ -15,21 +15,33 @@
 # https://www.datacamp.com/tutorial/wordcloud-python
 
 import itertools                                # slice and dice to pull the top results from the dic
+from tkinter import filedialog                  # for prompting user which files to be compared
 import matplotlib.pyplot as plt                 # for graphically ploting the results
 import re                                       # the regular expression snyax for finding all the words and dividing them up
+import tkinter                                  # used to ask user to select each file to be compared
+from datetime import date, timedelta            # used to create unique date stamp for file name
+from os import system, name                     # used to pull os info for clearing the screen
 from wordcloud import WordCloud                 # to convert results into a word cloud
 
 common_words = []                               # for the common words pulled from the related file
 file_common_words = 'common_words.txt'          # txt file with a list of common words to filter with
-file_INC_data_dump = 'data_dump.txt'            # txt file of the INC tickets to search through
+#file_INC_data_dump = 'data_dump.txt'            # txt file of the INC tickets to search through
 frequency = {}                                  # for the number of instances a word is used
 N_filter = 5                                    # remove any words with under this number of instances
 N_top = 50                                      # how many of the most frequent words to display
 sorted_report = {}                              # dic of filtered and high-to-low value sorted words and frequency counts
 word_report = {}                                # dic of all the words and frequency counts before filtering
 
+def clear():
+    # used to clear the text screen
+    # for windows
+    if name =='nt':
+        _ = system('cls')
+    # for linux & mac (here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
-def data_pull():
+def data_pull(file_INC_data_dump):
     # this opens the data dump file and pulls the info to be processed before closing the file
     # from the info we find all the words and add them to a list
     # the completed list is then returning for sorting and filtering
@@ -52,6 +64,7 @@ def data_count(match_pattern):
     for words in frequency_list:
         word_report[words] = frequency[words]
     
+    print('Total word count:     ', len(word_report))
     return word_report
 
 def data_filter(word_report):
@@ -68,7 +81,9 @@ def data_filter(word_report):
         for k in word_report.keys():
             if word_report[k] == i:
                 sorted_report[k] = word_report[k]
-
+    
+    print('Total unique words:   ', len(sorted_report))
+    
     with open(file_common_words, "r") as f:
         common_words = f.readlines()
         for w in common_words:
@@ -76,28 +91,52 @@ def data_filter(word_report):
                 if k == w.rstrip():
                     sorted_report.pop(k)
     
+    words_removed = int(len(word_report)) - int(len(sorted_report))
+    print('Common words removed: ', words_removed)
+
     filtered_report = dict(itertools.islice(sorted_report.items(), N_top))
 
     return filtered_report
 
 def word_cloud(filtered_report):
-    # here we are setting the parameters for creating the word cloud
-    # we then create the word cloud from the filtered dic and output/display the resulting image
-    # we can change this to auto-create an image export of the results if wanted at a later time
+    # first we create a unique ID based on the YYYY-MM-DD to be used for the file name
+    # then we set the parameters for creating the word cloud
+    # next we create the word cloud from the filtered dic and, formally, output/display the resulting image
+    # finally, we changed the code to not display result but instead auto-create an image export of the results
+    uniqueID = date.today() - timedelta(0)
     wc = WordCloud(background_color='white', width=1000, height=500, relative_scaling=0.5, normalize_plurals=False).generate_from_frequencies(filtered_report)
     plt.figure(figsize=(15,8))
     plt.imshow(wc)
     plt.axis('off')
     #plt.show()
-    plt.savefig('figure_1.png', format='png')
+    plt.savefig(str(uniqueID) + '_wordcloud.png', format='png')
+
+def main():
+    # here we run each func, get the returned data
+    # then hand the results to the next func to be processed again
+    # this is done until the final results are made and exported as a PNG word cloud
+    clear()
+    print('Please select the file with the newest info....\n')
+    tkinter.Tk().withdraw()
+
+    file_INC_data_dump_NEW = filedialog.askopenfilename(filetypes= [
+        ('txt files', '*.txt'),
+        ('csv file', '*.csv'),
+        ('json file', '*.json'),
+    ])
+
+    print('Running....\n')
+    match_pattern = data_pull(file_INC_data_dump_NEW)
+    word_report = data_count(match_pattern)
+    filtered_report = data_filter(word_report)
+
+    # this will need to be moved down further after 2 data lists have run, sorted, and filtered
+    # we'll then need to compare the 2 filtered reports to check for differences
+    # we can then create 2 export reports: 
+    #   1) A bar chart showing which words have become more-to-less frequent
+    #   2) A word cloud of the highest 10/20/50/100 uptrend in word freqency
+    #word_cloud(filtered_report)
 
 
-match_pattern = data_pull()
-word_report = data_count(match_pattern)
-filtered_report = data_filter(word_report)
-
-# was used for printing each result and frequency count line by line
-#for i in filtered_report:
-#    print(i,filtered_report[i])
-
-word_cloud(filtered_report)
+if __name__ == '__main__':
+    main()
