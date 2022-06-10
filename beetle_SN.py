5417011371aa31514a498128ace4e4e9b9d8a6ab
Brad Arrowood <61139407+bsarrowood@users.noreply.github.com>
2022-06-10 11:58:51 -0500

    Update beetle_SN.py
    editing notes

--- beetle_SN.py
+++ beetle_SN.py
@@ -2,7 +2,7 @@
 # Created on:       2022.05.09
 # Last updated:     2022.06.10
 # Script name:      beetle_SN.py
-# Description:      script to return top 5,000 for word frequency count of a file, less common words
+# Description:      script to return top 500 for word frequency count of a file, less common words
 #
 # Libraries Req.:   matplotlib
 #                   wordcloud
